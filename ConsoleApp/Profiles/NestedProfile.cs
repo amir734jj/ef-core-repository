1e8ca83edd23973435de47720c0ff6a91c693b94
Amir <hesamian@uwm.edu>
2020-12-28 00:16:29 -0600

    Working example of version 2.0.0

--- ConsoleApp/Profiles/NestedProfile.cs
+++ ConsoleApp/Profiles/NestedProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security;
 using ConsoleApp.Models;
 using EfCoreRepository.Interfaces;
@@ -7,5 +8,9 @@ namespace ConsoleApp.Profiles
 {
     public class NestedProfile : IEntityProfile<Nested>
     {
+        public IQueryable<Nested> Include<TQueryable>(TQueryable queryable) where TQueryable : IQueryable<Nested>
+        {
+            return queryable;
+        }
     }
 }
